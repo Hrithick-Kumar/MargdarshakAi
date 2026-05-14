97311cf0bb580cba7888720e0380a1a0b9801939
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-05-14 07:18:18 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -74,10 +74,10 @@ if st.button("Get Suggestion",use_container_width=True,type="primary"):
 # st.pyplot(p1)
 user_mark_data=pd.read_csv('StudentMark.csv',encoding="latin-1")
 df1=pd.DataFrame(user_mark_data)
-X1=df1['Average']
-y1=df1['Stream']
+inp=df1['Average']
+out=df1['Stream']
 model=DecisionTreeClassifier()
-model.fit(X1,y1)
+model.fit(inp,out)
 
 sub1=[]
 @st.dialog("User Input")
