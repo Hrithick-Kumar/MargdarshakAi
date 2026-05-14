d1806278e18daa6bcac9fa1752fc0d3c29dbb3f5
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-05-14 22:14:54 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -110,7 +110,7 @@ int_data={
        'Creativity':Creativity,
        'Stress_Handling_Level':Stress_Handling_Level
 }
-preference_prediction=model.predict([[int_data]])
+preference_prediction=model1.predict([[int_data]])
 st.toast(preference_prediction)
 @st.dialog("Update photo")
 def updatePhoto():
