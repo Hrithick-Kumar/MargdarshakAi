f69112c9b0a9505ebab120fc0bcaf015ff761be9
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-06-06 05:04:35 +0530

    Update MargdarshakAi.py

--- MargdarshakAi.py
+++ MargdarshakAi.py
@@ -45,7 +45,7 @@ st.markdown("""
 <h1 style="color:black;">Margdarshak AI</h1>
 <h3 style="color:black;">Your Smart Career & Stream Guidance Partner</h3>
 
-<div style="background:rgba(255,255,255,0.8);padding:20px;border-radius:20px;margin-top:20px;">
+<div style="background:rgba(25,25,25,0.8);padding:20px;border-radius:20px;margin-top:20px;">
 <h4>Explore your potential, discover your strengths, and make smarter academic decisions with Margdarshak AI.</h4>
 
 <p style="font-size:18px;">
