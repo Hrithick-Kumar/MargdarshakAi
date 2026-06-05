a1707c3da12932fad24a217748062fbc4167746c
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-06-05 19:07:49 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -105,13 +105,8 @@ if st.button("Predict"):
               final_prediction=Counter(result_list).most_common(1)[0][0]
               st.success(f"Final Recommended Stream: {final_prediction}")
               st.toast(f"Final Recommended Stream: {final_prediction}")
-              streamch=final_prediction
-@st.dialog("Visual Representation")
-def visual():
-       st.write(streamch)
-
-if st.button("Visual"):
-                     visual()              
+              plt.plot([10,20,30],["Science","Arts","Commerce"])
+              plt.show()
 
 @st.dialog("Update photo")
 def updatePhoto():
