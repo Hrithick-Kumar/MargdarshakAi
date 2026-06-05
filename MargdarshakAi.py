7c59d37d1c4ea967d6e59d212a16862e49580241
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-06-06 05:29:19 +0530

    Update MargdarshakAi.py

--- MargdarshakAi.py
+++ MargdarshakAi.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Margdarshak AI",page_icon="🎓",layout="wide")
 
 # ================= DATABASE =================
 
-conn=MongoClient(["MONGO_URI"])
+conn=MongoClient(st.secrets["MONGO_URI"])
 db=conn["User"]
 coll=db["Info"]
 
